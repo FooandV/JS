ddc6e530c7145ca4e3e4e22e5eb28204515ba129
Freyder Otalvaro <frotalva@bancolombia.com.co>
2019-08-27 15:58:28 -0500

    proyectos Nodejs
    proyectos Nodejs

---
+++ nodejs/app-blocking.js
@@ -0,0 +1,10 @@
+let {getUsuarioSync}= require('./usuarios/usuario');
+console.log('inicio de programa');
+
+let usuario1 = getUsuarioSync(1);
+console.log('Usuario1: ', usuario1);
+
+let usuario2 = getUsuarioSync(2);
+console.log('Usuario2: ', usuario2);
+
+console.log('Hola mundo!');
